69abe371dd44e6bc4e35e97962d37cc22e32aa62
jhonboy121 <alfredmathew05@gmail.com>
2022-03-04 16:19:46 -0800

    SystemUI: Cleanup smart space
    * I'm too lazy to write what I did now, kinda self-explanatory. Removed deprecated asynctask, unused imports, blah blah blah
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/pxosp/android/systemui/PxospSystemUIFactory.java
+++ packages/SystemUI/src/com/pxosp/android/systemui/PxospSystemUIFactory.java
@@ -2,7 +2,6 @@ package com.pxosp.android.systemui;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.os.Handler;
 
 import com.pxosp.android.systemui.dagger.PxospGlobalRootComponent;
 import com.pxosp.android.systemui.dagger.PxospSysUIComponent;
